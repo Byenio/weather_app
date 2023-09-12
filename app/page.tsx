2f1ed53be9b48434286218289ed0cc06bc2a5d58
Marcel Bieńko <57871338+Byenio@users.noreply.github.com>
2023-09-12 19:53:40 +0200

    Language update

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default async function Home() {
       </div>
 
       <CardHeader className="basis-full text-center">
-        <CardTitle>Pogoda {currentWeather.name}</CardTitle>
+        <CardTitle>Weather in {currentWeather.name}</CardTitle>
         <CardDescription>Data fetched using openweathermap API</CardDescription>
       </CardHeader>
 
@@ -57,10 +57,10 @@ export default async function Home() {
         </p>
         <div className="w-80 mx-auto">
           <Separator className="m-4 mx-auto" />
-          <p className="text-left"><b>Ciśnienie:</b> {currentWeather.main.pressure} hPa</p>
-          <p className="text-left"><b>Wilgotność:</b> {currentWeather.main.humidity}%</p>
+          <p className="text-left"><b>Pressure:</b> {currentWeather.main.pressure} hPa</p>
+          <p className="text-left"><b>Humidity:</b> {currentWeather.main.humidity}%</p>
           <p className="text-left">
-            <b>Wiatr:</b> {currentWeather.wind.speed}km/h
+            <b>Wind:</b> {currentWeather.wind.speed}km/h
             <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="inline mx-3"><g transform={`rotate(${currentWeather.wind.deg} 7.5 7.5)`}><path d="M11.1464 6.85355C11.3417 7.04882 11.6583 7.04882 11.8536 6.85355C12.0488 6.65829 12.0488 6.34171 11.8536 6.14645L7.85355 2.14645C7.65829 1.95118 7.34171 1.95118 7.14645 2.14645L3.14645 6.14645C2.95118 6.34171 2.95118 6.65829 3.14645 6.85355C3.34171 7.04882 3.65829 7.04882 3.85355 6.85355L7.5 3.20711L11.1464 6.85355ZM11.1464 12.8536C11.3417 13.0488 11.6583 13.0488 11.8536 12.8536C12.0488 12.6583 12.0488 12.3417 11.8536 12.1464L7.85355 8.14645C7.65829 7.95118 7.34171 7.95118 7.14645 8.14645L3.14645 12.1464C2.95118 12.3417 2.95118 12.6583 3.14645 12.8536C3.34171 13.0488 3.65829 13.0488 3.85355 12.8536L7.5 9.20711L11.1464 12.8536Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" /></g></svg>
           </p>
           <Separator className="m-4 mx-auto" />
@@ -69,7 +69,7 @@ export default async function Home() {
         <Accordion type="single" collapsible className="max-w-xs m-auto">
 
           <AccordionItem value="item-1">
-            <AccordionTrigger><b>Prognoza na najbliższe dni</b></AccordionTrigger>
+            <AccordionTrigger><b>Forecast for upcoming days</b></AccordionTrigger>
             <AccordionContent>
               {forecastWeather.list.map((element: any) => {
                 let newDayBool = false
